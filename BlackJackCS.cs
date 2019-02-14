e0f75ec47ff88857ea5edab866c1da42f324f131
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-14 14:34:04 +0800

    Update BlackJackCS.cs

--- BlackJackCS.cs
+++ BlackJackCS.cs
@@ -18,29 +18,11 @@ namespace BlackJackCS
                 tempChar = Console.ReadLine();
                 if (tempChar == "y")
                 {
-                    BlackJackObject.DrawCard();
-                    BlackJackObject.CountTotal();
+                    BlackJackObject.IfYes();
                 }
                 else if (tempChar == "n")
                 {
-                    if (BlackJackObject.totalNum < 11 && BlackJackObject.aceCount > 0)
-                    {
-                        Console.WriteLine(BlackJackObject.totalNum + 10);
-                    }
-                    else if (BlackJackObject.totalNum == 21 ||
-                             (BlackJackObject.totalNum == 11 && BlackJackObject.aceCount == 1))
-                    {
-                        Console.WriteLine("21");
-                    }
-                    else if (BlackJackObject.totalNum < 21)
-                    {
-                        Console.WriteLine(BlackJackObject.totalNum);
-                    }
-                    else if (BlackJackObject.totalNum > 21)
-                    {
-                        Console.WriteLine("Bust");
-                    }
-                        BlackJackObject.totalNum = 0;
+                    BlackJackObject.IfNo();
                 }
             }
         }
@@ -56,11 +38,73 @@ namespace BlackJackCS
         public static Random r2 = new Random();
         public int totalNum = 0;
         public int aceCount = 0;
+        public int decknum = 0;
+        public string[] cards = {"AH","2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
+        "AC","2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
+        "AD","2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
+        "AS","2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",};
+
         public void DrawCard()
-        {         
-            randomCard = r.Next(13);
-            randomSuite = r2.Next(4);
-            Console.WriteLine(deck[randomCard] + " of " + suite[randomSuite]);
+        {
+            for (int i = decknum; i < 52; i++)
+            {
+                Console.WriteLine(cards[i]);
+                decknum++;
+                break;
+            }
+        }
+
+        public void DisplayDeck() {
+            for (int i = 0; i < 52; i++)
+            {
+                Console.Write(cards[i] + " ");
+            }
+        }
+
+        public string[] Shuffle(string[] wordArray)
+        {
+            Random random = new Random();
+            for (int i = wordArray.Length - 1; i > 0; i--)
+            {
+                int swapIndex = random.Next(i + 1);
+                string temp = wordArray[i];
+                wordArray[i] = wordArray[swapIndex];
+                wordArray[swapIndex] = temp;
+            }
+            return wordArray;
+        }
+
+        public void IfNo() {
+            decknum = 0;
+            Shuffle(cards);
+            DisplayDeck();
+        }
+
+        public void IfYes() {
+            DrawCard();
+        }
+
+        public void CheckNo() {
+            {
+                if (totalNum < 11 && aceCount > 0)
+                {
+                    Console.WriteLine(totalNum + 10);
+                }
+                else if (totalNum == 21 ||
+                         (totalNum == 11 && aceCount == 1))
+                {
+                    Console.WriteLine("21");
+                }
+                else if (totalNum < 21)
+                {
+                    Console.WriteLine(totalNum);
+                }
+                else if (totalNum > 21)
+                {
+                    Console.WriteLine("Bust");
+                }
+                totalNum = 0;
+            }
         }
 
         public void CountTotal() {
@@ -108,7 +152,5 @@ namespace BlackJackCS
                     break;
             }
         }
-        
-
     }
 }
