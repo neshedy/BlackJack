68072163053a2e301cd704e2ac8ea23414c9d23b
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-15 10:54:26 +0800

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,131 +1,134 @@
 #include <iostream>
+
 using namespace std;
 
-int randnum;
-int cardtotal = 0;
-char drawcard;
-bool shouldplay = true;
-int numberofcards = 0;
-int acecount = 0, twocount = 0, threecount = 0, fourcount = 0,
-fivecount = 0, sixcount = 0, sevencount = 0, eightcount = 0,
-ninecount = 0, tencount = 0, jackcount = 0, queencount = 0,
-kingcount = 0;
-int ace = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, 
-	eight = 8, nine = 9, ten = 10, jack = 10, queen = 10, king = 10;
-int deck[13] = {ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king};
-
-class PlayerClass {
+class Card{
 	public:
-	void drawcardy() {
-		drawcardstart:
-		randnum = rand() % 13;
-		if (randnum == 0) {
-			acecount++;
-			if(acecount < 5)
-				cout << "ace" << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 1) {
-			twocount++;
-			if(twocount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 2) {
-			threecount++;
-			if(threecount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 3) {
-			fourcount++;
-			if(fourcount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 4) {
-			fivecount++;
-			if(fivecount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 5) {
-			sixcount++;
-			if(sixcount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 6) {
-			sevencount++;
-			if(sevencount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if (randnum == 7) {
-			eightcount++;
-			if(eightcount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if(randnum == 8) {
-			ninecount++;
-			if(eightcount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if(randnum == 9) {
-			tencount++;
-			if(tencount < 5)
-				cout << deck[randnum] << endl;
-			else goto drawcardstart;
-		}
-		if(randnum == 10) {
-			jackcount++;
-			if(jackcount < 5)
-				cout << "jack" << endl;
-			else goto drawcardstart;
-		}
-		if(randnum == 11) {
-			queencount++;
-			if(queencount < 5)
-				cout << "queen" << endl;
-			else goto drawcardstart;
-		}
-		if(randnum == 12) {
-			kingcount++;
-			if(kingcount < 5)
-				cout << "king" << endl;
-			else goto drawcardstart;
-		}
-		cardtotal += deck[randnum];
-	}
+		char type;
+		int value;
+		char suite;
 };
 
-void ifn() {
-	if (cardtotal == 21 ||
-		(cardtotal == 11 && acecount == 1))
-			cout << "21" << endl;
-	else if (cardtotal > 21)
-		cout << "bust" << endl;
-	else if (cardtotal < 21)
-		cout << cardtotal << endl;
-	cout << "acecount = " << acecount << endl;
-	cardtotal = 0;
-	acecount = 0;
-}
+class Player{
+	public:
+		int handValue;
+		int aceCount;
+};
+
+class Computer{
+	public:
+		Card card[52];
+		Player player;
+		int handValue = 0;
+		int aceCount = 0;
+		int cardCount = 0;
+		char suiteA[52] = {'D','C','H','S','D','C','H','S',
+		'D','C','H','S','D','C','H','S','D','C',
+		'H','S','D','C','H','S','D','C','H','S',
+		'D','C','H','S','D','C','H','S','D','C',
+		'H','S','D','C','H','S','D','C','H','S',
+		'D','C','H','S'};
+		char typeA[52] = {'A','2','3','4','5','6','7','8',
+		'9','T','J','Q','K','A','2','3','4','5','6','7','8',
+		'9','T','J','Q','K','A','2','3','4','5','6','7','8',
+		'9','T','J','Q','K','A','2','3','4','5','6','7','8',
+		'9','T','J','Q','K',};
+		void DealCard() {
+			for (cardCount; cardCount < 52; cardCount++) {
+				cout << card[cardCount].type << card[cardCount].suite << endl;
+				player.handValue += card[cardCount].value;
+				cardCount++;
+				break;
+			}	
+		}
+
+		void Shuffle() {
+			for (int i = 0; i < 52; i++) {
+				int r = rand() % 52;
+				int temp = suiteA[i];
+				suiteA[i] = suiteA[r];
+				suiteA[r] = temp;
+			}
+			for (int i = 0; i < 52; i++) {
+				int r = rand() % 52;
+				int temp = typeA[i];
+				typeA[i] = typeA[r];
+				typeA[r] = temp;
+			}
+			for (int i = 0; i < 52; i++) {
+				card[i].suite = suiteA[i];
+				card[i].type = typeA[i];
+			}
+		};
+		
+		void InitializeValue() {
+			for (int i = 0; i < 52; i++){
+				switch(card[i].type) {
+					case 'A': card[i].value = 1;
+					player.aceCount++;
+					break;
+					case '2': card[i].value = 2;
+					break;
+					case '3': card[i].value = 3;
+					break;
+					case '4': card[i].value = 4;
+					break;
+					case '5': card[i].value = 5;
+					break;
+					case '6': card[i].value = 6;
+					break;
+					case '7': card[i].value = 7;
+					break;
+					case '8': card[i].value = 8;
+					break;
+					case '9': card[i].value = 9;
+					break;
+					case 'T': card[i].value = 10;
+					break;
+					case 'J': card[i].value = 10;
+					break;
+					case 'Q': card[i].value = 10;
+					break;
+					case 'K': card[i].value = 10;
+					break;
+				}
+			}
+		};
+};
 
 int main() {
-	PlayerClass playerObj;
+	Computer computer;
+	computer.Shuffle();
+	computer.InitializeValue();
+	bool shouldplay = true;
 	while (shouldplay == true) {
-		cout << "Draw Card y/n" << endl;
-		cin >> drawcard;
-		if (drawcard == 'y') {
-			playerObj.drawcardy();
-		}
-		else if (drawcard == 'n') {
-			ifn();
-		}
+	char tempchar;
+	cin >> tempchar;
+	if (tempchar == 'y') {
+	computer.DealCard();
 	}
+	else if (tempchar == 'n') {
+                if (computer.player.handValue < 11 && computer.player.aceCount > 0)
+                {
+                    cout << computer.player.handValue + 10 << endl;
+                }
+                else if (computer.player.handValue == 21 ||
+                         (computer.player.handValue == 11 && computer.player.aceCount == 1))
+                {
+                    cout << "21" << endl;
+                }
+                else if (computer.player.handValue < 21)
+                {
+                    cout << computer.player.handValue << endl;
+                }
+                else if (computer.player.handValue > 21)
+                {
+                    cout << "Bust" << endl;
+                }
+                computer.player.handValue = 0;
+				computer.player.aceCount = 0;
+			}
+		}
 	int tempint;
 	cin >> tempint;
 	return 0;
