7422253dafeaa0e89fb4884b167305d360fef6e7
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-16 07:22:02 +0800

    Create BlackJackJAVA.java

---
+++ BlackJackJAVA.java
@@ -0,0 +1,18 @@
+/*
+ * A simple blackjack program in java
+ * 
+ * 
+ */
+package blackjackjava;
+
+/**
+ *
+ * @author neshman
+ */
+public class BlackJackJAVA {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
