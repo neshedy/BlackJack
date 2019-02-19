6180486647216d2b6352e1156a2a75d353021d0e
neshedy <35135350+neshedy@users.noreply.github.com>
2019-02-19 13:37:48 +0800

    Update BlackJackJAVA.java

--- BlackJackJAVA.java
+++ BlackJackJAVA.java
@@ -1,18 +1,29 @@
 /*
  * A simple blackjack program in java
  * 
- * 
  */
 package blackjackjava;
 
-/**
- *
- * @author neshman
- */
+import java.util.Scanner;
+
 public class BlackJackJAVA {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Dealer dealer = new Dealer();
+        System.out.println("y to deal card, n to pass, s to shuffle");
+        boolean shouldplay = true;
+        while (shouldplay == true) {
+            char tempchar;
+            Scanner s = new Scanner(System.in);
+            tempchar = (s.nextLine()).charAt(0);
+            switch(tempchar) {
+                case 'y': dealer.IfY();
+                break;
+                case 'n': dealer.IfN();
+                break;
+                case 's': {dealer.Shuffle(); dealer.InitializeValue();}
+                break;
+            }
+        }
     }
-    
 }
